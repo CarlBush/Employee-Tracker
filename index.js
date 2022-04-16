5e47297e106baaeebcb2ae76b7e73bb5f8f6edef
Carl Bush <CarlBushs13@gmail.com>
2022-04-16 13:19:24 -0700

    employee,department, and role classes created

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquirer = require("inquirer");
 
+const Department = require("./lib/department");
+const Role = require("./lib/role");
+const Employee = require("./lib/employee");
+
 //FIRST MAIN PROMPT
 const mainPrompt = () => {
     return inquirer.prompt([
