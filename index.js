6cc6ba1829a12a7de7e2e21f2369eaea0ec07c84
Carl Bush <CarlBushs13@gmail.com>
2022-04-18 10:36:26 -0700

    fix async queries

--- index.js
+++ index.js
@@ -30,52 +30,50 @@ const mainPrompt = () => {
         //     type: "input",
         //     name: "department",
         //     message: "What is the name of the department?",
-        //     when: (answer) =>answer.mainPrompt ==="Add Department"
+        //     when: (answer) => answer.mainPrompt === "Add Department"
         // },
         //PROMPT "Add Role" | ADD NAME OF ROLE
         {
             type: "input",
             name: "role",
             message: "What is the name of the role?",
-            when: (answer) =>answer.mainPrompt ==="Add Role"
+            when: (answer) => answer.mainPrompt === "Add Role"
         },
         //PROMPT "Add Role" | ADD SALARY OF ROLE
         {
             type: "input",
             name: "salary",
             message: "What is the salary of the role?",
-            when: (answer) =>answer.mainPrompt ==="Add Role"
+            when: (answer) => answer.mainPrompt === "Add Role"
         },
         //PROMPT "Add Employee" | ADD FIRST NAME
         {
             type: "input",
             name: "firstname",
             message: "What is the employee's first name?",
-            when: (answer) =>answer.mainPrompt ==="Add Employee"
+            when: (answer) => answer.mainPrompt === "Add Employee"
         },
         //PROMPT "Add Employee" | ADD LAST NAME
         {
             type: "input",
             name: "lastname",
             message: "What is the employee's last name?",
-            when: (answer) =>answer.mainPrompt ==="Add Employee"
+            when: (answer) => answer.mainPrompt === "Add Employee"
         },
     ]).then(responses => {
         switch(responses.mainPrompt) {
             case "View All Departments":
-                viewDepartment();
-                break;
-            case "Add Department" :
-                promptAddDepartment();
-                break;
+                return viewDepartment();
+            case "Add Department":
+                return promptAddDepartment();
             case "View All Roles":
-                viewRole();
-                break;
+                return viewRole();
             case "View All Employees":
-                viewEmployee();
-                break;
+                return viewEmployee();
         };
-    });
+    }).then(() => {
+        mainPrompt();
+    })
 };
 
 mainPrompt();
